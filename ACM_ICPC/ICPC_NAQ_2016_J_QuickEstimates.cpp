71c883421bdf74a90242d766ccf5b3f000dda1d8
JerryHu87 <56461496+JerryHu87@users.noreply.github.com>
2019-10-11 22:40:05 -0400

    Update ICPC_NAQ_2016_J_QuickEstimates.cpp

--- ACM_ICPC/ICPC_NAQ_2016_J_QuickEstimates.cpp
+++ ACM_ICPC/ICPC_NAQ_2016_J_QuickEstimates.cpp
@@ -1,1 +1,13 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(0);cin.tie(0);
+    int n; cin >> n;string a;
+    for(int i = 0;i<n;i++){
+        cin >> a;
+        cout << a.size() << "\n";
+    }
+    return 0;
+}
