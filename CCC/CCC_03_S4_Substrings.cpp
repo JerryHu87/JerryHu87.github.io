b39dc178e273bf85bea680afc190e26c166acfbe
JerryHu87 <56461496+JerryHu87@users.noreply.github.com>
2019-10-11 23:46:23 -0400

    Update CCC_03_S4_Substrings.cpp

--- CCC/CCC_03_S4_Substrings.cpp
+++ CCC/CCC_03_S4_Substrings.cpp
@@ -4,22 +4,32 @@
 #include <string>
 using namespace std;
 
-int main() {
-  int n;
-  scanf("%d", &n);
-  for(int i = 0;i<n;i++){
-    string str;
-    cin >> str;
-    unordered_set <string> s;
-    int num = 0;
-    for(int j = 1;j<str.size();j++){
-      for(int r = 0;r<=str.size()-j;r++){
-        s.insert(str.substr(r, j));
-      }
-      num += s.size();
-      s.clear();
+long long int pw[100001];
+long long int n,cur,num;
+string str;
+
+void init(){
+    pw[0] = 1;
+    for(int i = 1;i<=100000;i++){
+    	pw[i] = pw[i-1]*131;
     }
-    cout << num + 2 << endl;
-  }
+}
+int main() {
+	cin.sync_with_stdio(0);cin.tie(0);
+	cin >> n;
+	init();
+	for(int i = 0;i<n;i++){
+		cin >> str;
+		num = 0;
+		unordered_set <long long int> s;
+		for(int j = 0;j<(int)str.size();j++){
+			cur = 0;
+			for(int r = 0;r<=(int)str.size()-j;r++){
+				cur = cur+pw[r]*str[j+r];
+				s.insert(cur);
+			}
+		}
+		cout << s.size()+1 << "\n";
+	}
   return 0;
 }
