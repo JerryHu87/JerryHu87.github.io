9be6faa34cbc22b48929c0e1e5c57ea2895dda5c
JerryHu87 <56461496+JerryHu87@users.noreply.github.com>
2019-10-12 00:43:38 -0400

    Create CCC_06_J1_CanadianCalorieCounting.py

---
+++ CCC/CCC_06_J1_CanadianCalorieCounting.py
@@ -0,0 +1,31 @@
+total = 0
+a = int(input())
+if a == 1:
+    total = total + 461
+elif a == 2:
+    total = total + 431
+elif a == 3:
+    total = total + 420
+a = int(input())
+if a == 1:
+    total = total + 100
+elif a == 2:
+    total = total + 57
+elif a == 3:
+    total = total + 70
+a = int(input())
+if a == 1:
+    total = total + 130
+elif a == 2:
+    total = total + 160
+elif a == 3:
+    total = total + 118
+a = int(input())
+if a == 1:
+    total = total + 167
+elif a == 2 :
+    total = total + 266
+elif a == 3:
+    total = total + 75
+answer = "Your total Calorie count is " + str(total) + "."
+print(answer)
