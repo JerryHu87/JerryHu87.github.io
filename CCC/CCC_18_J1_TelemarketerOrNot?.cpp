ebf9331dda889b24795d058afc879a4d39c92dfe
JerryHu87 <56461496+JerryHu87@users.noreply.github.com>
2019-10-24 14:23:25 -0400

    Create CCC_18_J1_TelemarketerOrNot?.cpp

---
+++ CCC/CCC_18_J1_TelemarketerOrNot?.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if(n != 8 && n != 9){
+        cout << "answer" << endl;
+        return 0;
+    }
+    int m;
+    scanf("%d", &n);
+    scanf("%d", &m);
+    if(n != m){
+        cout << "answer" << endl;
+        return 0;
+    }
+    scanf("%d", &n);
+    if(n != 8 && n != 9){
+        cout << "answer" << endl;
+        return 0;
+    }
+    cout << "ignore" << endl;
+    return 0;
+}
