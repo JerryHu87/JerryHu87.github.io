2ef744dfe2cce664a684b9ebacd08727c02fe2dc
JerryHu87 <56461496+JerryHu87@users.noreply.github.com>
2020-02-08 00:32:23 -0500

    Create MockCCO_18_C1_P6_ACombiningProblem.cpp

---
+++ CCO/MockCCO_18_C1_P6_ACombiningProblem.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<long long int,long long int>
+#define pdd pair<long double, long double>
+#define pb push_back
+void scan(int &number){bool negative = false;register int c;number = 0;c = getchar();if (c=='-'){negative = true;c = getchar();}for(;(c>47&&c<58);c=getchar())number = number *10 + c - 48;if (negative)number *= -1;}
+int di_; string dnms_, co_ = ",";
+void debug_(){cout<<endl;}
+template<typename F, typename... R> void debug_(F &f, R&... r){while(dnms_[di_] != ',')cout<<dnms_[di_++];di_++;cout<<": "<<f<<",";debug_(r...);}
+#define debug(...) dnms_=#__VA_ARGS__+co_,di_=0,debug_(__VA_ARGS__)
+const int MOD = 1000000007;
+const int INF = 0x3f3f3f3f;
+const long double PI = 3.14159265359;
+long long gcd(long long a, long long b) {return b == 0 ? a : gcd(b, a % b);}
+long long fpow(long long  b, long long exp, long long mod){if(exp == 0) return 1;long long t = fpow(b,exp/2,mod);if(exp&1) return t*t%mod*b%mod;return t*t%mod;}
+long long divmod(long long i, long long j, long long mod){i%=mod,j%=mod;return i*fpow(j,mod-2,mod)%mod;}
+long long lcm(long long a, long long b){return a*b/gcd(a,b);}
+
+int n,a;
+deque<int> dq;
+ll ans;
+
+int main(){
+	//freopen("file.in","r",stdin); freopen("file.out","w",stdout);
+	cin.sync_with_stdio(0);cin.tie(0);
+	//cout << setprecision(2);cout << fixed;
+	cin >> n >> a;
+	dq.push_back(a);
+	for(int i = 1;i<n;i++){
+		cin >> a;
+		ans += max(a,dq.front());
+		dq.pop_front();
+		while(!dq.empty() && dq.back() >= a){dq.pop_back();}
+		dq.push_back(a);
+	}
+	cout << ans << "\n";
+	return 0;
+}
